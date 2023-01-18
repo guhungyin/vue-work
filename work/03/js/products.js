181f8bcde5ce8dd65140504ed2167ea207f3cbbd
ABOOS <hirosi840101@gmail.com>
2023-01-18 14:09:46 +0800

    第三周作業 完成

--- work/03/js/products.js
+++ work/03/js/products.js
@@ -1,11 +1,27 @@
+let ProductModal = null;
+let delProductModal = null;
 createApp({
     data() {
         return {
             apiUrl: 'https://vue3-course-api.hexschool.io/v2',
             apiPath:'aboos-work',
             products: [],
-            tempProduct: {},
+            isNew: false,
+            tempProduct: {
+                imagesUrl: [],
+            },
         }
+    },
+    mounted() {
+        ProductModal = new bootstrap.Modal(document.getElementById('ProductModal'), {
+            keyboard: false
+        })
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            keyboard: false
+        })
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)aboosToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+        this.checkAdmin();
     },
     methods: {
         checkAdmin(){
@@ -20,22 +36,58 @@ createApp({
             })
         },
         getData(){
-            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
-            axios.get(url)
-            .then((res) => {
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products/all`;
+            axios.get(url).then((res) => {
                 this.products = res.data.products;
-            })
-            .catch((err) => {
+            }).catch((err) => {
                 alert(err.response.data.message);
             })
         },
-        openProduct(item) {
-            this.tempProduct = item;
+        updateProduct(){
+            let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+            let http = 'post';
+
+            if(!this.isNew) {
+                url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+                http = 'put';
+            }
+            axios[http](url,{data: this.tempProduct}).then((res) => {
+                alert(res.data.message);
+                ProductModal.hide();
+                this.getData();
+            }).catch((err) => {
+                alert(err.response.data.message);
+            })
+        },
+        openModal(isNew,item){
+            if (isNew === 'new') {
+                this.tempProduct = {
+                    imagesUrl: [],
+                };
+                this.isNew = true;
+                ProductModal.show();
+            } else if (isNew === 'edit') {
+                this.tempProduct = {...item }
+                this.isNew = false;
+                ProductModal.show();
+            } else if (isNew === 'delet') {
+                this.tempProduct = {...item }
+                delProductModal.show();
+            }
+        },
+        delProduct(){
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+            axios.delete(url).then((res) => {
+                alert(res.data.message);
+                delProductModal.hide();
+                this.getData();
+            }).catch((err) => {
+                alert(err.response.data.message);
+            })
+        },
+        createImages(){
+            this.tempProduct.imagesUrl = [];
+            this.tempProduct.imagesUrl.push('');
         }
-    },
-    mounted() {
-        const token = document.cookie.replace(/(?:(?:^|.*;\s*)aboosToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-        axios.defaults.headers.common['Authorization'] = token;
-        this.checkAdmin();
-    },
+    }
 }).mount('#content');
